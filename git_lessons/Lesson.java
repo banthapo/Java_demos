2c12f6972a95569a66d893b5c5dc18929e5c9e6e
banthi <lchijuwa@gmail.com>
2024-12-19 12:31:05 +0200

    check conflicts

--- git_lessons/Lesson.java
+++ git_lessons/Lesson.java
@@ -4,5 +4,6 @@ public class Lesson {
     public static void main(String[] args){
         System.out.println("Trying out add and commit in the same command; then -> ");
         System.out.println("Committing on a different branch besides main :: dev; then -> ");
+        System.out.println("Checking conflicts :: main; then -> ");
     }
 }
