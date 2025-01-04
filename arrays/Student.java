ef047245aa49693f1b7d185614db47dccf956b3a
banthi <lchijuwa@gmail.com>
2025-01-04 20:48:23 +0200

    add student class

---
+++ arrays/Student.java
@@ -0,0 +1,37 @@
+package arrays;
+
+public class Student {
+    private String name;
+    private int id;
+    private float score;
+
+    public Student(String name, int id, float score) {
+        this.name = name;
+        this.id = id;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+}
